0dcbcda2c3d6f81371d015fc1fc009ebb0d40df6
李现成 <512157407@qq.com>
2019-04-30 10:44:48 +0800

    第二个文件

--- src/com/App.kt
+++ src/com/App.kt
@@ -1,8 +1,10 @@
 package com
 
 import com.lesson1.test
+import com.lesson1.test2
 
 fun main(){
     println("hello world")
     test()
+    test2()
 }
---
+++ src/com/lesson1/lesson1.kt
@@ -0,0 +1,5 @@
+package com.lesson1
+
+fun test2(){
+    println("i an test2")
+}
